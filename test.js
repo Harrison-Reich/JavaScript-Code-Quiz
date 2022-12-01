415aaadaa8b872bc391f1e7d6b8710a235240673
Harrison Reich <hreich1998@gmail.com>
2022-11-30 16:09:28 -0800

    questions

--- test.js
+++ test.js
@@ -1,5 +1,66 @@
+// array of questions
+const questions = [
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    },
+    {
+        question: "What is my favorite NFL team?",
+        choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
+        answer: 3
+    }
+]
+
+
+
+// start button function
 function startButton() {
-    let titleBox = document.getElementById('title-box');
-    titleBox.classList.add('hide')
-    console.log (titleBox)
+    document.getElementById('title-box').style.visibility = 'hidden';
+    console.log ('Begin quiz')
+}
+
+// set questions function
+function setQuestions() {
+    
 }
