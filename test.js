c5a294aa8a97e1d6c492fc604a533942f043756a
Harrison Reich <hreich1998@gmail.com>
2022-12-07 22:16:07 -0800

    code

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 // defining variables by ID
-const currentIndex = 0;
+let currentIndex = 0;
 const startButton = document.getElementById('start-button');
 const nextButton = document.getElementById('next-button');
 const questionTitleElement = document.getElementById('question-title');
 const answerChoicesElement = document.getElementById('answer-choices');
+var userpicks = []
+
 
 // array of questions
 const questions = [
@@ -11,43 +13,60 @@ const questions = [
         // question 1
         question: "Which team had the most recent Super Bowl? ",
         choices: ["The Packers", "The Saints", "The Broncos", "The Patriots"],
-        answer: 2
     },
     {
         // question 2
         question: "What is the best ice cream flavor?",
         choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answer: 3
     },
     {
         // question 3
         question: "What is brown and sticky?",
         choices: ["Pick this one", "Or this one", "Not this one", "Maybe this one"],
-        answer: 1
     },
+]
+
+const answers = [
+    3,0,2
 ]
 
 // when clicked, the start button will hide the code quiz window and show the question container while executing the startQuiz function
 startButton.addEventListener('click', () => {
     document.getElementById('title-box').classList.add('d-none');
     document.getElementById('questions-container').classList.remove('d-none');
-    startQuiz();
+    showQuestion();
 })
 
 // when called, startQuiz will get the current question index and populate the question title and answers according to the question index
-const startQuiz = () => {
-    let currentQuestion = questions[currentIndex];
-
-    questionTitleElement.innerHTML = currentQuestion.question
+const showQuestion = () => {
+    if (currentIndex === questions.length) {
+        endQuiz();
+    } else {
+        let currentQuestion = questions[currentIndex];
     
-    currentQuestion.choices.forEach(choice => {
-        answerChoicesElement.innerHTML += `<button class="btn btn-primary btnWidth">${choice}</button>`
-    });
+        questionTitleElement.innerHTML = currentQuestion.question
+        
+        currentQuestion.choices.forEach((choice, userchoice) => {
+            answerChoicesElement.innerHTML += `<button onclick="userpick(${userchoice})" class="btn btn-primary btnWidth">${choice}</button>`
+        });
+        nextButton.addEventListener('click', () => {
+            setNextQuestion();
+        })
+    }
+}
+
+const userpick = (answerpicked) => {
+    userpicks.push(answerpicked)
+    currentIndex++
     setNextQuestion();
+    showQuestion();
 }
 
 const setNextQuestion = () => {
-    nextButton.addEventListener('click', () => {
-        
-    })
+    questionTitleElement.innerHTML = ""
+    answerChoicesElement.innerHTML = ""
+}
+
+const endQuiz = () => {
+    document.getElementById('questions-container').classList.add('d-none');
 }
