d6005eaa30e78ad33fee2beeb543ae3d778887d0
Harrison Reich <hreich1998@gmail.com>
2022-12-08 19:38:18 -0800

    spacing and deleting code

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
 // defining variables by ID
-let currentIndex = 0;
-let userpicks = []
 const startButton = document.getElementById('start-button');
-const nextButton = document.getElementById('next-button');
 const questionTitleElement = document.getElementById('question-title');
 const answerChoicesElement = document.getElementById('answer-choices');
-
-
 // array of questions
 const questions = [
     {
@@ -60,19 +55,20 @@ const questions = [
         choices: ["The Denver Broncos", "The Las Vegas Raiders", "The Los Angeles Chargers", "The Kansas City Chiefs"],
     },
 ]
-
-// answer questions array
+// correct answers array
 const answers = [
     0,0,2,1,2,3,2,1,3,0
 ]
-
-// when clicked, the start button will hide the code quiz window and show the question container while executing the startQuiz function
+// current question index
+let currentIndex = 0;
+// array that will track user answer picks
+let userpicks = []
+// when clicked, the start button will hide the code quiz window and show the question container while executing the startButton function
 startButton.addEventListener('click', () => {
     document.getElementById('title-box').classList.add('d-none');
     document.getElementById('questions-container').classList.remove('d-none');
     showQuestion();
 })
-
 // when called, startQuiz will get the current question index and populate the question title and answers according to the question index
 const showQuestion = () => {
     if (currentIndex === questions.length) {
